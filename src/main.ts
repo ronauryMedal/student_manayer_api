89e93bfe91e30af7acb944285e426a395f13bb99
ronaury work <ronaury2010@gmail.com>
2026-05-05 12:57:06 -0400

    todas las tablas estan bien aplciadas

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Study Manager API')
+    .setDescription('Documentacion de la API para Study Manager')
+    .setVersion('1.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, swaggerDocument);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
